c856402d1f1cc26ad6bbe8e913b9540bd4d551be
조현제 <abb6575@naver.com>
2024-01-23 17:56:35 +0900

    집가서할거

---
+++ 조현제/longd-fe/src/App.vue
@@ -0,0 +1,30 @@
+<script src="openvidu-browser-VERSION.js"></script>
+<script src="app.js"></script>
+<script setup>
+import { OpenVidu } from 'openvidu-browser';
+var OV;
+var session;
+var mySessionId = document.getElementById('sessionId').value;
+
+OV = new OpenVidu();
+session = OV.initSession();
+
+session.on('streamCreated', function (event) {
+  session.subscribe(event.stream, 'subscriber');
+});
+
+var APPLICATION_SERVER_URL = 'http://localhost:5000/';
+
+function getToken(mySessionId) {
+  return createSession(mySessionId).then(sessionId => createToken)
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
