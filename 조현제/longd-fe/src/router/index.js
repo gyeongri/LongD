6178da6b893cbaf3c4e9acd7a9dbf1ab43493ccb
조현제 <abb6575@naver.com>
2024-01-28 11:48:20 +0900

    work

--- 조현제/longd-fe/src/router/index.js
+++ 조현제/longd-fe/src/router/index.js
@@ -4,11 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: '',
-      component: '',
-    },
+
   ],
 });
 
