5c872a6ebddec408182030fce8cc1a47b51d0ef7
조현제 <abb6575@naver.com>
2024-01-30 17:47:04 +0900

    [FE] remove

--- longd-fe/src/components/openvidu/ViduMine.vue
+++ longd-fe/src/components/openvidu/ViduMine.vue
@@ -2,6 +2,15 @@
   <ov-videoconference> </ov-videoconference>
   <div v-if="viduStore.publisher">
     <video ref="videoElement" autoplay></video>
+    <canvas
+      ref="canvas"
+      class="canvas"
+      width="640"
+      height="480"
+      @mousedown="startDrawing"
+      @mouseup="stopDrawing"
+      @mousemove="draw"
+    ></canvas>
   </div>
   <button @click="check">여기</button>
   <br />
@@ -17,6 +26,8 @@ import { computed, watch, ref } from 'vue';
 import { useViduStore } from '@/stores/vidu.js';
 const viduStore = useViduStore();
 const videoElement = ref(null);
+const canvas = ref(null);
+const isDrawing = ref(false);
 watch(
   () => viduStore.publisherTest,
   (newValue, oldValue) => {
@@ -52,10 +63,46 @@ const enterPiPMode = async () => {
     console.error('PiP 모드 진입 중 오류가 발생했습니다:', error);
   }
 };
+
+const startDrawing = () => {
+  isDrawing.value = true;
+};
+
+const stopDrawing = () => {
+  isDrawing.value = false;
+};
+const draw = event => {
+  if (!isDrawing.value) return;
+
+  const canvasElement = canvas.value;
+  const ctx = canvasElement.getContext('2d');
+
+  // 마우스 위치에서 선 그리기
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+
+  ctx.lineTo(
+    event.clientX - canvasElement.offsetLeft,
+    event.clientY - canvasElement.offsetTop,
+  );
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(
+    event.clientX - canvasElement.offsetLeft,
+    event.clientY - canvasElement.offsetTop,
+  );
+};
 </script>
 
 <style scoped>
 .video {
   border: 1px solid black;
+}
+.canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  cursor: crosshair;
 }
 </style>
