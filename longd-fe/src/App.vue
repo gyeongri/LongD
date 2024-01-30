84b767dc0e2351986a5de0560e9149ad1ac429c6
조현제 <abb6575@naver.com>
2024-01-30 23:07:51 +0900

    [FE] Feat pip유지

--- longd-fe/src/App.vue
+++ longd-fe/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <TheHeader></TheHeader>
-
+  <ViduMainView class="hihi"></ViduMainView>
   <TheView></TheView>
 </template>
 <script setup>
 import TheHeader from '@/layouts/TheHeader.vue';
 import TheView from '@/layouts/TheView.vue';
+import ViduMainView from './views/openvidu/ViduMainView.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.hihi {
+  visibility: hidden;
+  height: 1px;
+  width: 1px;
+  display: flex;
+}
+</style>
