66e1fc19037bdca89e12a9c7f302078cd3059888
손동천 <kij8025@naver.com>
2024-02-08 03:24:12 +0900

    [BE] 강제수정

--- longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
+++ longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -21,13 +23,13 @@ public class FileUploadController {
     FileUploadService fileUploadService;
 
 
-    @PostMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> postImage (@RequestParam("file") MultipartFile file) {
+    @PostMapping(value = "/uploads", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> postObjectMany (@RequestParam("file")List<MultipartFile> file) {
         // 파일 이름 출력 (예제에서는 콘솔에 출력)
-        System.out.println("Received file: " + file.getOriginalFilename());
-        String tmp = "";
+
+        List<String> tmp = new ArrayList<>();
         try {
-            tmp = fileUploadService.uploadImageToS3(file);
+            tmp = fileUploadService.uploadObjectToS3Many(file);
         } catch (IOException e) {
             System.out.println(e.toString());
         }
@@ -36,5 +38,6 @@ public class FileUploadController {
         // 성공적인 응답
         return ResponseEntity.ok(tmp);
     }
+
 
 }
