553bb3fca3217c254b67d5507f557e51accc66dd
손동천 <kij8025@naver.com>
2024-02-07 18:17:09 +0000

    Revert "[BE] 다중 업로드 구현"
    This reverts commit 77cfac24ea9ff552fd299526cc7f86bcf37a9ced.
    
    Reason for revert: 머지가 안됨
    
    Change-Id: I862b4baf23ae47136c3fde965f1f999d5b442c16

--- longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
+++ longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/fileUpload")
@@ -27,22 +26,6 @@ public class FileUploadController {
         String tmp = "";
         try {
             tmp = fileUploadService.uploadImageToS3(file);
-        } catch (IOException e) {
-            System.out.println(e.toString());
-        }
-        if(tmp.equals("")) return ResponseEntity.status(503).body("Fail");
-
-        // 성공적인 응답
-        return ResponseEntity.ok(tmp);
-    }
-
-    @PostMapping(value = "/imageMany", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> postImageMany (@RequestParam("file")List<MultipartFile> file) {
-        // 파일 이름 출력 (예제에서는 콘솔에 출력)
-
-        String tmp = "";
-        try {
-            tmp = fileUploadService.uploadImageToS3Many(file);
         } catch (IOException e) {
             System.out.println(e.toString());
         }
