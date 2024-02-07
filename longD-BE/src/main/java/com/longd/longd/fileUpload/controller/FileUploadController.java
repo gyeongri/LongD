dc9936fa1ee5555151e4e81659dcaac5c2c3d685
손동천 <kij8025@naver.com>
2024-02-07 23:08:45 +0900

    [BE] FEAT 다중업로드 구현

--- longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
+++ longD-BE/src/main/java/com/longd/longd/fileUpload/controller/FileUploadController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/fileUpload")
@@ -26,6 +27,22 @@ public class FileUploadController {
         String tmp = "";
         try {
             tmp = fileUploadService.uploadImageToS3(file);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        if(tmp.equals("")) return ResponseEntity.status(503).body("Fail");
+
+        // 성공적인 응답
+        return ResponseEntity.ok(tmp);
+    }
+
+    @PostMapping(value = "/imageMany", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> postImageMany (@RequestParam("file")List<MultipartFile> file) {
+        // 파일 이름 출력 (예제에서는 콘솔에 출력)
+
+        String tmp = "";
+        try {
+            tmp = fileUploadService.uploadImageToS3Many(file);
         } catch (IOException e) {
             System.out.println(e.toString());
         }
