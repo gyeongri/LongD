3b132c6cb7de28fd7679e809462d4743d3ee8c9f
손동천 <kij8025@naver.com>
2024-02-08 14:48:36 +0900

    [BE] 작업환경 추가

--- longD-BE/src/main/java/com/longd/longd/user/config/CorsConfig.java
+++ longD-BE/src/main/java/com/longd/longd/user/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://i10d206.p.ssafy.io:3001", "http://192.168.100.102:5173", "http://192.168.100.103:5173", "http://192.168.100.188:5173", "http://192.168.0.2:5173", "http://192.168.0.39:5173/"));
+        config.setAllowedOrigins(List.of("http://i10d206.p.ssafy.io:3001", "http://192.168.100.102:5173", "http://192.168.100.103:5173", "http://192.168.100.188:5173", "http://192.168.0.2:5173", "http://192.168.0.39:5173/", "http://192.168.100.116:5173/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
