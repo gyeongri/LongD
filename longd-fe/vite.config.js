78a7b90014293dadf7b1594b7c1b5940176c646c
KimIkHwan <hw147@naver.com>
2024-02-09 14:34:18 +0900

    [FE]_Bug_server ssl 추가시 계속 새로고침이 발생하여 삭제

--- longd-fe/vite.config.js
+++ longd-fe/vite.config.js
@@ -10,11 +10,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    hmr: {
-      host: 'i10d206.p.ssafy.io',
-      port: 443, // SSL을 사용한다면 443, 그렇지 않다면 80 또는 필요한 포트로 설정
-      protocol: 'wss', // SSL을 사용한다면 'wss', 그렇지 않다면 'ws'
-    }
-  }
 });
