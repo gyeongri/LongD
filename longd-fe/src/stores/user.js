cbd572b8f203fd564db1a2b14d7774cb83a85876
손동천 <kij8025@naver.com>
2024-02-10 00:04:52 +0900

    Merge branch 'develop' of https://i10d206.p.ssafy.io:8989/a/S10P12D206 into develop
    Change-Id: I6ec581ee8d147a959e432fd95eef85e458381ec5

--- longd-fe/src/stores/user.js
+++ longd-fe/src/stores/user.js
@@ -6,12 +6,16 @@ export const useUserStore = defineStore('user', () => {
     userState.value = state;
   };
   const isLogin = computed(
+<<<<<<< HEAD   (1df2c7 Merge branch 'develop' of https://i10d206.p.ssafy.io:8989/a/)
     () =>
       !(
         userState.value === '롱디에 로그인 되어 있지 않음' ||
         userState.value === '' ||
         userState.value === null
       ),
+=======
+    () => userState.value !== '롱디에 로그인 되어 있지 않음',
+>>>>>>> CHANGE (610089 Merge branch 'develop' of https://i10d206.p.ssafy.io:8989/a/)
   );
   return { userState, setUserState, isLogin };
 });
