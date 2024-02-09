46a9c10f0f38e38d7c5bee0d2a78bfa9f9b59943
KimIkHwan <hw147@naver.com>
2024-02-09 13:07:36 +0900

    [FE]_Style_환경변수 임포트

--- longd-fe/src/layouts/TheChatting.vue
+++ longd-fe/src/layouts/TheChatting.vue
@@ -19,6 +19,7 @@ import ChatInputView from '@/views/chat/ChatInputView.vue';
 import ChatDisplayView from '@/views/chat/ChatDisplayView.vue';
 import { onMounted, reactive, ref } from 'vue';
 import { stompApi } from '@/utils/api/index.js';
+const { VITE_CHAT_BASE_IP } = import.meta.env;
 
 const coupleId = ref(77);
 const messages = reactive([]);
