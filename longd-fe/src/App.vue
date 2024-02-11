bac012632e84ffd1ac06cf0f8f8e28695f21e760
조현제 <abb6575@naver.com>
2024-02-11 17:25:49 +0900

    [FE]chat임시 주석

--- longd-fe/src/App.vue
+++ longd-fe/src/App.vue
@@ -9,15 +9,15 @@
         <TheView></TheView>
       </div>
 
-      <div v-show="userStore.isLogin">
+      <!-- <div v-show="userStore.isLogin">
         <div v-if="isChatting" class="border-4 border-red-300 w-1/4">
           <TheChatting></TheChatting>
         </div>
         <div v-else class="border-4 border-red-300 w-1/10">
           <TheNochatting></TheNochatting>
-        </div>
-        <ViduMainView class="hihi"></ViduMainView>
-      </div>
+        </div> -->
+      <ViduMainView class="hihi"></ViduMainView>
+      <!-- </div> -->
     </div>
   </div>
 </template>
