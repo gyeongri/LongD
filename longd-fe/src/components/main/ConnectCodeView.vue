5cdc67adf4d326ecb2719c3d35c948eaec01cf1c
이정민 <bona3377@naver.com>
2024-02-13 01:26:08 +0900

    [FE]_Fix_connectCode

--- longd-fe/src/components/main/ConnectCodeView.vue
+++ longd-fe/src/components/main/ConnectCodeView.vue
@@ -127,22 +127,29 @@ const choiceDate = async () => {
   coupleMatching(
     checkInfo.value,
     success => {
+      console.log(success.data);
       // 4가지 경우의 수
       if (success.data === '코드가 일치하지 않습니다.') {
         Swal.fire(
           '코드가 일치하지 않습니다. 상대방이 전해준 코드가 맞는지 확인해주세요.',
         );
+        console.log(checkInfo.value);
       }
       if (success.data === '상대방 이름 또는 생일이 일치하지 않습니다.') {
-        Swal.fire('동일한 정보를 가진 사람이 없습니다. 다시 확인해주세요.');
+        Swal.fire(
+          '상대방 이름 또는 생일이 일치하지 않습니다. 다시 확인해주세요.',
+        );
+        console.log(checkInfo.value);
       }
       if (success.data === '상대방이 coupleListId를 가지고 있는 상태입니다.') {
         Swal.fire('이미 다른 사람과 연결된 사람입니다. 다시 확인해주세요.');
+        console.log(checkInfo.value);
       }
       if (success.data === '상대방이 존재하지 않습니다.') {
         Swal.fire(
           '동일한 이메일 정보를 가진 사람이 없습니다. 다시 확인해주세요.',
         );
+        console.log(checkInfo.value);
       }
       if (success.data === '커플리스트를 만드는데 성공했습니다.') {
         // 커플리스트 정보조회&수정
