50ed1ee14bbe023d0dc0a86dbbe6a2aff0ad2800
KimIkHwan <hw147@naver.com>
2024-02-13 14:24:32 +0900

    [BE]_Style_front cors

--- longd-openvidu/src/main/java/io/openvidu/recording/java/config/WebSecurityConfig.java
+++ longd-openvidu/src/main/java/io/openvidu/recording/java/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://i10d206.p.ssafy.io:3001", "http://192.168.100.102:5173", "http://192.168.100.103:5173", "http://192.168.100.188:5173", "http://192.168.0.2:5173", "http://192.168.0.39:5173/", "http://192.168.100.116:5173/","http://192.168.236.200:5173/","http://192.168.236.188:5173/","http://192.168.0.6:5173/","http://localhost:3001","http://192.168.100.116:3001/"));
+        config.setAllowedOrigins(List.of("https://i10d206.p.ssafy.io","http://i10d206.p.ssafy.io:3001", "http://192.168.100.102:5173", "http://192.168.100.103:5173", "http://192.168.100.188:5173", "http://192.168.0.2:5173", "http://192.168.0.39:5173/", "http://192.168.100.116:5173/","http://192.168.236.200:5173/","http://192.168.236.188:5173/","http://192.168.0.6:5173/","http://localhost:3001","http://192.168.100.116:3001/"));
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
