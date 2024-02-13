ce7988d49327a153e942718cb1c9baf6f8ead252
전근렬 <hm04137@naver.com>
2024-02-13 14:55:18 +0900

    [FE]feat vidu design plan

--- longd-fe/src/components/plan/MapSearch.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>검색화면 나오게하기</div>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
--- longd-fe/src/layouts/TheView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main>
-    <RouterView></RouterView>
-  </main>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
