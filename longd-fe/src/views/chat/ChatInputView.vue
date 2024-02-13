ba6b68a5745fa09fff509ac9f184d864f0039a21
조현제 <abb6575@naver.com>
2024-02-13 09:11:19 +0900

    [FE]chat클릭이벤트누락

--- longd-fe/src/views/chat/ChatInputView.vue
+++ longd-fe/src/views/chat/ChatInputView.vue
@@ -7,6 +7,7 @@
       class="flex-1 py-2 px-3 rounded-full bg-gray-100 focus:outline-none"
     />
     <button
+      @click="sendMessageToMain"
       class="bg-blue-500 text-white px-4 py-2 rounded-full ml-3 hover:bg-blue-600"
     >
       전송
