42b4c11e4b12191823d70632a890456f2df45d8a
정경리 <wjdrudfl413@gmail.com>
2024-02-14 02:31:07 +0900

    [Infra] synctube start.sh 내용 분리

--- longd-SyncTube/start.sh
+++ longd-SyncTube/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# npm install 실행
-npm install
+# # npm install 실행
+# npm install
 
-# npm start 실행
-npm start &
+# # npm start 실행
+# npm start &
 
 # Nginx 실행
 nginx -g "daemon off;"
