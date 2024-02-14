00f5dcf0bffa2cf7d59c8642d1af3b028604a1e8
손동천 <kij8025@naver.com>
2024-02-14 13:39:36 +0900

    [BE] PLAN 삭제 로직 수정

--- longD-BE/src/main/java/com/longd/longd/plan/service/PlanSerivce.java
+++ longD-BE/src/main/java/com/longd/longd/plan/service/PlanSerivce.java
@@ -11,7 +11,7 @@ public interface PlanSerivce {
 
     public boolean setPlan(Plan plan);
 
-    public boolean deletePlan(int id);
+    public String deletePlan(int id);
 
     public Plan getDetailPlan(int planId);
 }
