c6e351af777d3dc57d6e0a8ed074290a0b78af03
이정민 <bona3377@naver.com>
2024-02-14 16:52:29 +0900

    [FE]_design_header style

--- longd-fe/src/components/app/AppDropdown.vue
+++ longd-fe/src/components/app/AppDropdown.vue
@@ -1,30 +1,9 @@
 <template>
-  <!-- <details class="dropdown dropdown-end">
-    <summary class="btn btn-circle btn-ghost text-pink-400">
-      <svg
-        class="swap-off fill-current"
-        xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        viewBox="0 0 512 512"
-      >
-        <path
-          d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"
-        />
-      </svg>
-    </summary>
-
-    <ul
-      class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
-    >
-      <slot></slot>
-    </ul>
-  </details> -->
   <div class="dropdown dropdown-end">
     <div
       tabindex="0"
       role="button"
-      class="btn btn-circle btn-ghost text-pink-400"
+      class="btn btn-circle btn-ghost text-black-400"
     >
       <svg
         class="swap-off fill-current"
@@ -42,11 +21,11 @@
       tabindex="0"
       class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-[7rem]"
     >
-      <slot></slot>
+      <slot @click="handleSlotClick"></slot>
     </ul>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
