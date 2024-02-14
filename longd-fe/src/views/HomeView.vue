405589d19935de6fb1cd9574c9ae7d47781677b8
이정민 <bona3377@naver.com>
2024-02-14 20:43:11 +0900

    [FE]_Fix_삭제된 거 복구

--- longd-fe/src/views/HomeView.vue
+++ longd-fe/src/views/HomeView.vue
@@ -30,24 +30,52 @@
         </div>
       </figure>
     </div>
-    <div>
-      <label class="btn btn-secondary-content" for="img">메인 사진 변경</label>
-      <input
-        type="file"
-        id="img"
-        autocomplete="img"
-        @change="changImg"
-        hidden
-      />
+
+    <div class="flex justify-end mb-10 mr-10">
+      <div class="flex">
+        <AppDropdown>
+          <template v-slot>
+            <li class="font-bold lis">
+              <div>
+                <label for="img">메인 사진 변경</label>
+                <input
+                  type="file"
+                  id="img"
+                  autocomplete="img"
+                  @change="changImg"
+                  hidden
+                />
+              </div>
+            </li>
+            <li class="font-bold lis" @click="changeDday()">
+              <a>처음 만난 날 변경</a>
+            </li>
+            <li class="font-bold lis" @click="unConnectLove()">
+              <a>상대와 연결 끊기</a>
+            </li>
+            <li class="font-bold lis" @click="deleteMyData()">
+              <a>회원 탈퇴</a>
+            </li>
+          </template>
+        </AppDropdown>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import AppDropdown from '@/components/app/AppDropdown.vue';
 import { ref, onMounted, watchEffect } from 'vue';
-import { partnerinfo, coupleDataGet, coupleDataModify } from '@/utils/api/user';
+import {
+  partnerinfo,
+  coupleDataGet,
+  coupleDataModify,
+  BrokenLove,
+  DeleteUser,
+} from '@/utils/api/user';
 import { uploadImage } from '@/utils/api/photo';
 import { useUserStore } from '@/stores/user.js';
+import Swal from 'sweetalert2';
 import dayjs from 'dayjs';
 
 const partnerInfo = ref({});
@@ -82,6 +110,112 @@ const changImg = event => {
       console.log('사진을 변환할 수 없어요.', error);
     },
   );
+};
+
+const changeDday = () => {
+  coupleDataGet(
+    success => {
+      coupleInfo.value = success.data;
+      console.log(coupleInfo.value);
+    },
+    async success2 => {
+      const { value: date } = await Swal.fire({
+        title: '여러분이 처음 만난 날을 입력해주세요.',
+        input: 'date',
+        didOpen: () => {},
+        preConfirm: () => {
+          const selectedDate = Swal.getInput().value;
+          const today = new Date().toISOString().split('T')[0];
+          console.log(selectedDate);
+          if (!selectedDate) {
+            Swal.showValidationMessage('날짜를 선택해주세요');
+            return false;
+          } else if (selectedDate > today) {
+            Swal.showValidationMessage('만난일은 미래일 수 없습니다.');
+            return false;
+          }
+        },
+      });
+      if (date) {
+        await Swal.fire('여러분의 처음 만난 날', date);
+        coupleInfo.value.startDay = date;
+        startDay.value = dayjs(date);
+        await coupleDataModify(
+          coupleInfo.value,
+          success => {
+            console.log(coupleInfo.value);
+          },
+          error => {
+            Swal.fire('수정되지않았어요!', error);
+            console.log('커플 정보가 수정되지 않음');
+          },
+        );
+      } else {
+        console.log(coupleInfo.value);
+        Swal.fire('날짜가 입력되지 않았어요.');
+      }
+    },
+    error => {
+      console.log('커플리스트 정보 조회 불가능', error);
+    },
+  );
+};
+
+const unConnectLove = () => {
+  Swal.fire({
+    title:
+      '상대와 연결을 끊으실건가요? 연결을 끊으면 우리의 모든 추억이 삭제됩니다.',
+    showCancelButton: true,
+    allowEscapeKey: false,
+    confirmButtonText: '그래도 연결 끊기',
+    cancelButtonText: '다시 생각해보기',
+  }).then(result => {
+    if (result.isConfirmed) {
+      console.log('상대와 연결을 끊는다고 했다..');
+      BrokenLove(
+        success => {
+          console.log('연결끊기 완료');
+        },
+        error => {
+          console.log('연결끊기 실패', error);
+        },
+      );
+    }
+  });
+};
+
+const deleteMyData = () => {
+  // 진짜 회원탈퇴할건지 물어보기
+  Swal.fire({
+    title:
+      '회원탈퇴를 원하시나요? 회원탈퇴를 하면 롱디와의 모든 추억이 삭제됩니다.',
+    showCancelButton: true,
+    allowEscapeKey: false,
+    confirmButtonText: '회원 탈퇴',
+    cancelButtonText: '다시 생각해보기',
+  }).then(result => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: '정말로 회원탈퇴하실건가요?.',
+        showCancelButton: true,
+        allowEscapeKey: false,
+        confirmButtonText: '변함없이 회원탈퇴',
+        cancelButtonText: '다시 롱디와 함께하기',
+      }).then(result => {
+        if (result.isConfirmed) {
+          console.log('진짜 회원탈퇴를 한다고 했다..');
+          DeleteUser(
+            success => {
+              console.log('회원탈퇴 완료');
+            },
+            error => {
+              console.log('회원탈퇴 실패', error);
+            },
+          );
+        }
+      });
+    }
+  });
 };
 
 onMounted(() => {
