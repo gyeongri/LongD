11a12abe8e40a991a5db869c6d985eb863633fd3
조현제 <abb6575@naver.com>
2024-02-15 00:08:53 +0900

    [FE]button조작감개선 2

--- longd-fe/src/components/plan/MapMenu.vue
+++ longd-fe/src/components/plan/MapMenu.vue
@@ -5,12 +5,17 @@
       <div class="drawer-side">
         <div class="menu p-4 w-80 min-h-full bg-base-200 text-base-content"> -->
     <div class="flex justify-center gap-1 border-b border-red-200 mb-1">
-      <button class="btn btn-sm mr-1" style="background-color: #fac1c1">
-        <RouterLink :to="{ name: 'MapSearch' }">장소검색</RouterLink>
-      </button>
-      <button class="btn btn-sm mr-1" style="background-color: #fac1c1">
-        <RouterLink :to="{ name: 'MapPlan' }">일정짜기</RouterLink>
-      </button>
+      <RouterLink :to="{ name: 'MapSearch' }">
+        <button class="btn btn-sm mr-1" style="background-color: #fac1c1">
+          장소검색
+        </button></RouterLink
+      >
+
+      <RouterLink :to="{ name: 'MapPlan' }">
+        <button class="btn btn-sm mr-1" style="background-color: #fac1c1">
+          일정짜기
+        </button></RouterLink
+      >
     </div>
     <div class="routerview">
       <RouterView></RouterView>
