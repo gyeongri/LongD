17b1fa10daff97149d30d65ae870a87de217eab2
전근렬 <hm04137@naver.com>
2024-02-15 23:49:25 +0900

    fix bug

--- longd-fe/src/router/index.js
+++ longd-fe/src/router/index.js
@@ -42,14 +42,6 @@ const router = createRouter({
       component: ConnectCodeView,
     },
     {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/profile',
       name: 'Profile',
       component: ProfileView,
